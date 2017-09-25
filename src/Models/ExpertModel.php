1a162e3a2770f93990fc394e25325a8ee8e95395
Konstantin Bukin <bukin@inetstudio.ru>
2017-09-26 01:41:18 +0300

    Add fit manipulation

--- src/Models/ExpertModel.php
+++ src/Models/ExpertModel.php
@@ -141,7 +141,7 @@ class ExpertModel extends Model implements HasMediaConversions
             foreach (config('experts.images.conversions') as $collection => $image) {
                 foreach ($image as $crop) {
                     foreach ($crop as $conversion) {
-                        $imageConversion = $this->addMediaConversion($conversion['name'])->quality($quality);
+                        $imageConversion = $this->addMediaConversion($conversion['name']);
 
                         if (isset($conversion['size']['width'])) {
                             $imageConversion->width($conversion['size']['width']);
@@ -149,6 +149,17 @@ class ExpertModel extends Model implements HasMediaConversions
 
                         if (isset($conversion['size']['height'])) {
                             $imageConversion->height($conversion['size']['height']);
+                        }
+
+                        if (isset($conversion['fit']['width']) && isset($conversion['fit']['height'])) {
+                            $imageConversion->fit('max', $conversion['fit']['width'], $conversion['fit']['height']);
+                        }
+
+                        if (isset($conversion['quality'])) {
+                            $imageConversion->quality($conversion['quality']);
+                            $imageConversion->optimize();
+                        } else {
+                            $imageConversion->quality($quality);
                         }
 
                         $imageConversion->performOnCollections($collection);
