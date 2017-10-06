1042d8d317b592cc7ad56f77a9eb1b2dff136943
Konstantin Bukin <bukin@inetstudio.ru>
2017-10-06 16:29:55 +0300

    Code optimize

--- src/Controllers/ExpertsController.php
+++ src/Controllers/ExpertsController.php
@@ -6,11 +6,13 @@ use Illuminate\Http\Request;
 use Yajra\Datatables\Datatables;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Storage;
 use InetStudio\Experts\Models\ExpertModel;
+use InetStudio\AdminPanel\Traits\DatatablesTrait;
 use InetStudio\Experts\Requests\SaveExpertRequest;
 use Cviebrock\EloquentSluggable\Services\SlugService;
 use InetStudio\Experts\Transformers\ExpertTransformer;
+use InetStudio\AdminPanel\Traits\MetaManipulationsTrait;
+use InetStudio\AdminPanel\Traits\ImagesManipulationsTrait;
 
 /**
  * Контроллер для управления экспертами.
@@ -19,6 +21,10 @@ use InetStudio\Experts\Transformers\ExpertTransformer;
  */
 class ExpertsController extends Controller
 {
+    use DatatablesTrait;
+    use MetaManipulationsTrait;
+    use ImagesManipulationsTrait;
+
     /**
      * Список экспертов.
      *
@@ -27,61 +33,9 @@ class ExpertsController extends Controller
      */
     public function index(Datatables $dataTable)
     {
-        $table = $dataTable->getHtmlBuilder();
-
-        $table->columns($this->getColumns());
-        $table->ajax($this->getAjaxOptions());
-        $table->parameters($this->getTableParameters());
+        $table = $this->generateTable($dataTable, 'experts', 'index');
 
         return view('admin.module.experts::pages.index', compact('table'));
-    }
-
-    /**
-     * Свойства колонок datatables.
-     *
-     * @return array
-     */
-    private function getColumns()
-    {
-        return [
-            ['data' => 'name', 'name' => 'name', 'title' => 'Имя'],
-            ['data' => 'created_at', 'name' => 'created_at', 'title' => 'Дата создания'],
-            ['data' => 'updated_at', 'name' => 'updated_at', 'title' => 'Дата обновления'],
-            ['data' => 'actions', 'name' => 'actions', 'title' => 'Действия', 'orderable' => false, 'searchable' => false],
-        ];
-    }
-
-    /**
-     * Свойства ajax datatables.
-     *
-     * @return array
-     */
-    private function getAjaxOptions()
-    {
-        return [
-            'url' => route('back.experts.data'),
-            'type' => 'POST',
-            'data' => 'function(data) { data._token = $(\'meta[name="csrf-token"]\').attr(\'content\'); }',
-        ];
-    }
-
-    /**
-     * Свойства datatables.
-     *
-     * @return array
-     */
-    private function getTableParameters()
-    {
-        return [
-            'paging' => true,
-            'pagingType' => 'full_numbers',
-            'searching' => true,
-            'info' => false,
-            'searchDelay' => 350,
-            'language' => [
-                'url' => asset('admin/js/plugins/datatables/locales/russian.json'),
-            ],
-        ];
     }
 
     /**
@@ -176,123 +130,11 @@ class ExpertsController extends Controller
         $item->save();
 
         $this->saveMeta($item, $request);
-        $this->saveImages($item, $request, ['og_image', 'preview', 'content']);
+        $this->saveImages($item, $request, ['og_image', 'preview', 'content'], 'experts');
 
         Session::flash('success', 'Эксперт «'.$item->name.'» успешно '.$action);
 
         return redirect()->to(route('back.experts.edit', $item->fresh()->id));
-    }
-
-    /**
-     * Сохраняем мета теги.
-     *
-     * @param ExpertModel $item
-     * @param SaveExpertRequest $request
-     */
-    private function saveMeta($item, $request)
-    {
-        if ($request->has('meta')) {
-            foreach ($request->get('meta') as $key => $value) {
-                $item->updateMeta($key, $value);
-            }
-
-            \Event::fire('inetstudio.seo.cache.clear', $item);
-        }
-    }
-
-    /**
-     * Сохраняем изображения.
-     *
-     * @param ExpertModel $item
-     * @param SaveExpertRequest $request
-     * @param array $images
-     */
-    private function saveImages($item, $request, $images)
-    {
-        foreach ($images as $name) {
-            $properties = $request->get($name);
-
-            \Event::fire('inetstudio.images.cache.clear', $name.'_'.md5(get_class($item).$item->id));
-
-            if (isset($properties['images'])) {
-                $item->clearMediaCollectionExcept($name, $properties['images']);
-
-                foreach ($properties['images'] as $image) {
-                    if ($image['id']) {
-                        $media = $item->media->find($image['id']);
-                        $media->custom_properties = $image['properties'];
-                        $media->save();
-                    } else {
-                        $filename = $image['filename'];
-
-                        $file = Storage::disk('temp')->getDriver()->getAdapter()->getPathPrefix().$image['tempname'];
-
-                        $media = $item->addMedia($file)
-                            ->withCustomProperties($image['properties'])
-                            ->usingName(pathinfo($filename, PATHINFO_FILENAME))
-                            ->usingFileName($image['tempname'])
-                            ->toMediaCollection($name, 'experts');
-                    }
-
-                    $item->update([
-                        $name => str_replace($image['src'], $media->getFullUrl('content_front'), $item[$name]),
-                    ]);
-                }
-            } else {
-                $manipulations = [];
-
-                if (isset($properties['crop']) and config('experts.images.conversions')) {
-                    foreach ($properties['crop'] as $key => $cropJSON) {
-                        $cropData = json_decode($cropJSON, true);
-
-                        foreach (config('experts.images.conversions.'.$name.'.'.$key) as $conversion) {
-
-                            \Event::fire('inetstudio.images.cache.clear', $conversion['name'].'_'.md5(get_class($item).$item->id));
-
-                            $manipulations[$conversion['name']] = [
-                                'manualCrop' => implode(',', [
-                                    round($cropData['width']),
-                                    round($cropData['height']),
-                                    round($cropData['x']),
-                                    round($cropData['y']),
-                                ]),
-                            ];
-                        }
-                    }
-                }
-
-                if (isset($properties['tempname']) && isset($properties['filename'])) {
-                    $image = $properties['tempname'];
-                    $filename = $properties['filename'];
-
-                    $item->clearMediaCollection($name);
-
-                    array_forget($properties, ['tempname', 'temppath', 'filename']);
-                    $properties = array_filter($properties);
-
-                    $file = Storage::disk('temp')->getDriver()->getAdapter()->getPathPrefix().$image;
-
-                    $media = $item->addMedia($file)
-                        ->withCustomProperties($properties)
-                        ->usingName(pathinfo($filename, PATHINFO_FILENAME))
-                        ->usingFileName($image)
-                        ->toMediaCollection($name, 'experts');
-
-                    $media->manipulations = $manipulations;
-                    $media->save();
-                } else {
-                    $properties = array_filter($properties);
-
-                    $media = $item->getFirstMedia($name);
-
-                    if ($media) {
-                        $media->custom_properties = $properties;
-                        $media->manipulations = $manipulations;
-                        $media->save();
-                    }
-                }
-            }
-        }
     }
 
     /**
