4bb495ee9daec3418169b9ed9ae38fa54f532e06
Konstantin Bukin <bukin@inetstudio.ru>
2018-03-05 17:07:03 +0300

    Fixes

--- src/Transformers/Back/SuggestionTransformer.php
+++ src/Transformers/Back/SuggestionTransformer.php
@@ -42,7 +42,7 @@ class SuggestionTransformer extends TransformerAbstract implements SuggestionTra
             $modelClass = get_class($item);
 
             return [
-                'value' => $item->title,
+                'value' => $item->name,
                 'data' => [
                     'id' => $item->id,
                     'type' => $modelClass,
