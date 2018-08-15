8668023e05a10f057efa2f1fd497241e4c084c5b
Konstantin Bukin <bukin@inetstudio.ru>
2018-08-15 16:14:08 +0300

    Change bindings provider

--- src/Providers/PersonsBindingsServiceProvider.php
+++ src/Providers/PersonsBindingsServiceProvider.php
@@ -3,33 +3,47 @@
 namespace InetStudio\Persons\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class PersonsBindingsServiceProvider.
  */
 class PersonsBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * PersonsBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\Persons\Contracts\Events\Back\ModifyPersonEventContract' => 'InetStudio\Persons\Events\Back\ModifyPersonEvent',
+        'InetStudio\Persons\Contracts\Http\Controllers\Back\PersonsControllerContract' => 'InetStudio\Persons\Http\Controllers\Back\PersonsController',
+        'InetStudio\Persons\Contracts\Http\Controllers\Back\PersonsDataControllerContract' => 'InetStudio\Persons\Http\Controllers\Back\PersonsDataController',
+        'InetStudio\Persons\Contracts\Http\Controllers\Back\PersonsUtilityControllerContract' => 'InetStudio\Persons\Http\Controllers\Back\PersonsUtilityController',
+        'InetStudio\Persons\Contracts\Http\Requests\Back\SavePersonRequestContract' => 'InetStudio\Persons\Http\Requests\Back\SavePersonRequest',
+        'InetStudio\Persons\Contracts\Http\Responses\Back\Persons\DestroyResponseContract' => 'InetStudio\Persons\Http\Responses\Back\Persons\DestroyResponse',
+        'InetStudio\Persons\Contracts\Http\Responses\Back\Persons\FormResponseContract' => 'InetStudio\Persons\Http\Responses\Back\Persons\FormResponse',
+        'InetStudio\Persons\Contracts\Http\Responses\Back\Persons\IndexResponseContract' => 'InetStudio\Persons\Http\Responses\Back\Persons\IndexResponse',
+        'InetStudio\Persons\Contracts\Http\Responses\Back\Persons\SaveResponseContract' => 'InetStudio\Persons\Http\Responses\Back\Persons\SaveResponse',
+        'InetStudio\Persons\Contracts\Http\Responses\Back\Utility\SlugResponseContract' => 'InetStudio\Persons\Http\Responses\Back\Utility\SlugResponse',
+        'InetStudio\Persons\Contracts\Http\Responses\Back\Utility\SuggestionsResponseContract' => 'InetStudio\Persons\Http\Responses\Back\Utility\SuggestionsResponse',
+        'InetStudio\Persons\Contracts\Models\PersonModelContract' => 'InetStudio\Persons\Models\PersonModel',
+        'InetStudio\Persons\Contracts\Observers\PersonObserverContract' => 'InetStudio\Persons\Observers\PersonObserver',
+        'InetStudio\Persons\Contracts\Repositories\PersonsRepositoryContract' => 'InetStudio\Persons\Repositories\PersonsRepository',
+        'InetStudio\Persons\Contracts\Services\Back\PersonsDataTableServiceContract' => 'InetStudio\Persons\Services\Back\PersonsDataTableService',
+        'InetStudio\Persons\Contracts\Services\Back\PersonsObserverServiceContract' => 'InetStudio\Persons\Services\Back\PersonsObserverService',
+        'InetStudio\Persons\Contracts\Services\Back\PersonsServiceContract' => 'InetStudio\Persons\Services\Back\PersonsService',
+        'InetStudio\Persons\Contracts\Services\Front\PersonsServiceContract' => 'InetStudio\Persons\Services\Front\PersonsService',
+        'InetStudio\Persons\Contracts\Transformers\Back\PersonTransformerContract' => 'InetStudio\Persons\Transformers\Back\PersonTransformer',
+        'InetStudio\Persons\Contracts\Transformers\Back\SuggestionTransformerContract' => 'InetStudio\Persons\Transformers\Back\SuggestionTransformer',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
