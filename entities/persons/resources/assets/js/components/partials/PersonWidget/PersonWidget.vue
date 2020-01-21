f1a86f7ffe267e347c469aabcb7060d0c12bbb46
Konstantin Bukin <bukin@inetstudio.ru>
2020-01-21 17:04:21 +0300

    Fixes

--- entities/persons/resources/assets/js/components/partials/PersonWidget/PersonWidget.vue
+++ entities/persons/resources/assets/js/components/partials/PersonWidget/PersonWidget.vue
@@ -29,7 +29,7 @@
                         <base-wysiwyg
                             label = "Текст"
                             name = "person_opinion"
-                            v-bind:value.sync="model.additional_info.opinion"
+                            v-bind:value.sync="model.params.opinion"
                             v-bind:simple = true
                             v-bind:attributes = "{
                                 'id': 'person_opinion',
@@ -66,7 +66,7 @@
 
             axios.get(url).then(response => {
               $(component.$refs.person_suggestion.$refs.autocomplete).val(response.data.name).trigger('change');
-              window.tinymce.get('person_opinion').setContent(component.model.additional_info.opinion);
+              window.tinymce.get('person_opinion').setContent(component.model.params.opinion);
 
               component.options.loading = false;
             });
