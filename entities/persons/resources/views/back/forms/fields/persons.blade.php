9aeb774348282397b06096e4368eb83ef017b49e
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-24 19:27:45 +0300

    Update to 7.0

--- entities/persons/resources/views/back/forms/fields/persons.blade.php
+++ entities/persons/resources/views/back/forms/fields/persons.blade.php
@@ -11,18 +11,19 @@
     $userPerson = $personsService->getItemsByUserId($userID)->pluck('name', 'id')->toArray();
 @endphp
 
-{!! Form::dropdown('persons[]', (empty($value)) ? array_keys($userPerson) : $value, [
+{!! Form::dropdown(($name ?? 'persons').'[]', (empty($value)) ? array_keys($userPerson) : $value, [
     'label' => [
         'title' => $attributes['label'] ?? 'Персоны',
     ],
-    'field' => [
+    'field' => array_merge([
         'class' => 'select2-drop form-control',
         'data-placeholder' => $attributes['placeholder'] ?? 'Выберите персон',
         'style' => 'width: 100%',
-        'multiple' => 'multiple',
         'data-source' => route('back.persons.getSuggestions'),
         'data-exclude' => isset($attributes['exclude']) ? implode('|', $attributes['exclude']) : '',
-    ],
+    ], ((! isset($attributes['field']['multiple'])) || isset($attributes['field']['multiple']) && $attributes['field']['multiple']) ? ['multiple' => 'multiple'] : [],
+       (isset($attributes['field']['readonly']) && $attributes['field']['readonly']) ? ['readonly' => 'readonly'] : []
+    ),
     'options' => [
         'values' => (empty($options)) ? $userPerson : $options,
     ],
